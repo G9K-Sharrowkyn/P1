d35dff0151248b1b70fab639e78b823ff4bd3a5e
Karl Wittelsbach <ak4maaru@gmail.com>
2023-09-25 02:41:39 +0000

    App.css

---
+++ App.css
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Master of Mankind</h1>
+        <img src={process.env.PUBLIC_URL + '/o8ojRiutfKxpZDPR.jpg'} alt="zdjecie" />
+        <a
+          className="App-link"
+          href="https://www.reddit.com/r/Grimdank/comments/157bsj5/the_end_and_the_death_of_the_greatest_friendship/?utm_source=share&utm_medium=mweb3x&utm_name=mweb3xcss&utm_term=1&utm_content=share_button"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          The End and the Death
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
