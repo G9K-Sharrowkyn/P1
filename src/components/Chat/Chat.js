4bd53ec536f4eedded700cbb99d9c1cfeb51a26e
Zero <ak4maaru@gmail.com>
2023-10-18 13:33:54 +0200

    com

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -21,3 +21,18 @@ export default function Chat() {
     </div>
   );
 }
+
+/*
+Importowanie modułów. 
+React to moduł główny.
+useState to funkcja pozwalająca na korzystanie z hooka stanu w komponencie
+Następnie wczytywane są dwa moduły lokalne
+
+Deklaracja komponentu chat.
+Inicjujemy stan komponentu za pomocą hooka useState. Zmienna 'bla' przechowuje tablicę poczatkowych wartości 'setBla'.
+
+Funkcja addMessage dodaje nowe wiadomości do stanu 'bla', dodaje wiadomości do istniejącej tablicy 'bla'.
+
+Komponent chat składa się z kontenera <div>, nagłówka <h1>, komponentu ChatMessageContainer, który otrzymuje tablicę 'bla' jako właściwość 'messages'
+oraz komponentu ChatForm, który otrzymuje funkcję 'addMessage' jako właściwość.
+*/
