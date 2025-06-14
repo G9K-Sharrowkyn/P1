323bb161ef334546ff626e37c354f67274d611ec
G9K-Sharrowkyn <ak4maaru@gmail.com>
2025-06-14 04:46:58 +0200

    Update Final 0.5

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createMemoryHistory } from 'history';
 import App from './App';
+import configureStore from './store'; // <-- To Twój configureStore z reducerami
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app', () => {
+  const history = createMemoryHistory();
+  const store = configureStore({}, history); // pusty stan początkowy i historia
+
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  // Możesz dodać konkretny test, np.
+  // expect(screen.getByText(/Coś tam/i)).toBeInTheDocument();
 });
